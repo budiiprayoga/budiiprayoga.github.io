b4d21a2c8fc62ec578a112bd83039fd227c976ce
budiiprayoga <budiyogatara12@gmail.com>
2023-09-16 19:39:18 +0700

    add feature auto flip avatar when page reload

--- js/flipAvatar.js
+++ js/flipAvatar.js
@@ -1,18 +1,36 @@
 // flip avatar
-// Mengambil elemen-elemen yang diperlukan
-const avatarBox = document.querySelector('.avatar-box');
+// Mengambil elemen yang diperlukan
+document.addEventListener("DOMContentLoaded", function() {
+    const avatarBox = document.querySelector('.avatar-box');
+    let flipTimeout;
+    let isHovered = false;
 
-function flipOnPageReload() {
-    // Menghapus kelas .flipped jika sudah ada
-    avatarBox.classList.remove('flipped-on-load');
+// Fungsi untuk memutar avatar ke gambar kedua
+    function flipAvatar() {
+        avatarBox.style.transform =  "rotateY(180deg)";
+        if (!isHovered) {
+            flipTimeout = setTimeout(resetAvatar, 1200); // Kembali ke posisi semula setelah 1.2 detik
+        }
+    }
 
-    // Menambahkan kelas .flipped setelah sedikit penundaan
-    setTimeout(() => {
-        avatarBox.classList.add('flipped-on-load');
-    }, 1000); // 1000ms (1 detik) penundaan sebelum flip
-}
+    // Fungsi untuk mengembalikan avatar ke posisi semula
+    function resetAvatar() {
+        avatarBox.style.transform = "rotateY(0deg)";
+    }
 
-// Memanggil fungsi flipOnPageLoad saat halaman dimuat
-window.addEventListener('load', flipOnPageReload);
+    // Memanggil fungsi flipAvatar() secara otomatis saat halaman dimuat
+    flipAvatar();
+
+    // Event listener untuk memicu efek flip saat avatar dihover
+    avatarBox.addEventListener("mouseenter", function () {
+        clearTimeout(flipTimeout); // Menghapus timeout saat dihover
+        isHovered = true;
+        flipAvatar(); // Memutar avatar ke gambar kedua
+    });
+
+    avatarBox.addEventListener("mouseleave", function () {
+        resetAvatar(); //mengembalikan avatar ke semula
+    });
+});
 
 // ----------------------------------------------
